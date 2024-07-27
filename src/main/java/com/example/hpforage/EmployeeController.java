95dc8e7fa72936b5c4aa088e90462d02c4e28b62
Rajorshi27 <sarker.rajorshi@gmail.com>
2024-07-27 13:25:57 -0400

    all

--- src/main/java/com/example/hpforage/EmployeeController.java
+++ src/main/java/com/example/hpforage/EmployeeController.java
@@ -1,16 +1,21 @@
-package com.example.restservice;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
+
     @Autowired
     private EmployeeManager employeeManager;
 
     @GetMapping("/employees")
-    public Employees getEmployees() {
-        return employeeManager.getEmployees();
+    public List<Employee> getEmployees() {
+        return employeeManager.getEmployees().getEmployeeList();
+    }
+
+    @PostMapping("/employees")
+    public void addEmployee(@RequestBody Employee employee) {
+        employeeManager.getEmployees().getEmployeeList().add(employee);
     }
 }
